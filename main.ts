172a3c38d449df1fd5a29d48272167a11cf26bb7
ampcpmgp <ampcpmgp@gmail.com>
2023-05-21 14:44:54 +0900

    llm

--- main.ts
+++ main.ts
@@ -1,11 +1,18 @@
 import { load } from "https://deno.land/std@0.188.0/dotenv/mod.ts";
+import { OpenAI } from "https://esm.sh/langchain@0.0.78/llms/openai";
 
 const env = await load();
-start();
+llm();
 
-async function start() {
-  console.log(
-    "🚀🐡 ~ file: main.ts:5 ~ start ~ API_KEY:",
-    env["OPENAI_API_KEY"]
+async function llm() {
+  const model = new OpenAI({
+    openAIApiKey: env["OPENAI_API_KEY"],
+    temperature: 0.9,
+  });
+
+  const res = await model.call(
+    "What would be a good company name a company that makes colorful socks?"
   );
+
+  console.log(res);
 }
